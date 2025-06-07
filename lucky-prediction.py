d7f3c237bbe282923b7a84f371ecadf1c5cdca09
Vitalina <vitalinafromre43@gmail.com>
2025-06-07 14:01:11 +0200

    Create lucky-prediction.py
    first commit

---
+++ lucky-prediction.py
@@ -0,0 +1,5 @@
+import random
+predictions=["One day you’ll understand everything. But not today.",'Chaos suits you. Own it.','No plan? Perfect. Do it freestyle.','Fortune is hiding in your second sock.','Time is fake. You’re not late.','You will make a choice that rewires your path.','A window will open where you expected a wall.','Something tiny will make you wildly happy.']
+selected=random.choice(predictions)
+print('Your fortune for today:')
+print(selected)
