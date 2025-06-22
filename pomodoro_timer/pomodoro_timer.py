714ce060f1e352441d1bc056dbbf7b066260cd36
Vitalina <vitalinafromre43@gmail.com>
2025-06-22 12:45:40 +0200

    Update pomodoro_timer.py
    Enable user to define focos/break cycles

--- pomodoro_timer/pomodoro_timer.py
+++ pomodoro_timer/pomodoro_timer.py
@@ -5,15 +5,25 @@ def countdown(minutes):
         mins=total_seconds//60
         secs=total_seconds%60
         print(f'Time left:{mins:02d}:{secs:02d}',end='\r')
-        time.sleep(1)
+        time.sleep(0.1)
         total_seconds-=1
     else:
         print("Time's up!        ")
+def pomodoro(focus_minutes, break_minutes, intervals):
+    for i in range(intervals):
+        print(f'\nSession {i+1}\nTime to focus!')
+        countdown(focus_minutes)
+        if i<intervals - 1:
+            print('Break time!')
+            countdown(break_minutes)
+    print('\nPomodoro session complete!')
 if __name__=='__main__':
     try:
-        user_input=int(input('Enter time in minutes: ').strip())
-        if user_input>0:
-            countdown(user_input)
+        intervals=int(input('How many focus sessions? ').strip())
+        focus=int(input('Minutes per focus session: ').strip())
+        brk=int(input('Minutes per break: ').strip())
+        if intervals>0 and focus>0 and brk>0:
+            pomodoro(focus, brk, intervals)
         else:
             print("Please enter a positive number.")
     except:
