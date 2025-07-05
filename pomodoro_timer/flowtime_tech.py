0cab50ce3fcf52860c847afd7aaeea26daacac10
Vitalina <vitalinafromre43@gmail.com>
2025-07-05 16:01:19 +0200

    Update flowtime_tech.py
    Draft version of the flowtime technique stopwatch - works but needs cleanup

--- pomodoro_timer/flowtime_tech.py
+++ pomodoro_timer/flowtime_tech.py
@@ -1,18 +1,39 @@
 import time
 print('Hey! Ready to dive into work?')
-intervals=[]
+focus_intervals=[]
+break_intervals=[]
+focus_TIME=[]
+break_TIME=[]
 print("Press ENTER to start and stop each interval. Type 'q' tnen ENTER to quit.\n")
 while True:
-    start_input=input("Start timing (or 'q' to quit):")
-    if start_input.lower()=='q':
+    focus_input=input("Time to focus(Press ENTER to start or 'q' to quit):")
+    if focus_input.lower()=='q':
+        stop_time=time.time()
+        break_TIME.append(stop_time)
         break
-    start_time=time.time()
-    stop_input=input("Stop timing: ")
-    elapsed=time.time()-start_time
-    intervals.append(elapsed)
-    print(f"Interval recorded: {elapsed:.2f}seconds\n")
-    if stop_input.lower()=='q':
+    focus_time=time.time()
+    focus_TIME.append(focus_time)
+    if focus_input.lower()=='q':
+        stop_time=time.time()
+        break_TIME.append(stop_time)
+        break
+    break_input=input("Time for a break (Press ENTER to start or 'q' to quit): ")
+    break_time=time.time()
+    break_TIME.append(break_time)
+    if break_input.lower()=='q':
         break
 print("\nAll intervals:")
-for i, t in enumerate(intervals,1):
-    print(f"Interval {i}: {t:.2f} seconds")
+if len(break_TIME)>len(focus_TIME):
+    del break_TIME[-1]
+for i in range(len(focus_TIME)):
+    focus_intervals.append(break_TIME[i]-focus_TIME[i])
+    if focus_TIME[i]==focus_TIME[-1]:
+        break
+    break_intervals.append(focus_TIME[i+1]-break_TIME[i])
+break_intervals.append(0)
+for i, f, b in zip(range(len(focus_intervals)),focus_intervals, break_intervals):
+    print(f"Focus: {f:.2f} seconds")
+    if i==len(focus_intervals)-1:
+        break
+    print(f"Break: {b:.2f} seconds")
+
